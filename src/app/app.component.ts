ae09afe7fb936756df415bb86b116d8d82b49ee7
Administrator <admin@example.com>
2021-07-03 04:21:27 +0430

    ngif and ngfor directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,22 @@ export class AppComponent implements OnInit{
   styles = {};
   classes = {};
 
+  users: string[] = ['faryad', 'farzad', 'payam'];
 
+  usersObject = [
+    {
+      id: 1,
+      name: 'faryad'
+    },
+    {
+      id: 2,
+      name: 'faryad2222'
+    },
+    {
+      id: 3,
+      name: 'faryad33333'
+    },
+  ];
   ngOnInit(): void {
     this.styles = {
       'font-size': this.isCorrect ? '2rem' : '8rem',
@@ -22,5 +37,16 @@ export class AppComponent implements OnInit{
       'big-font-size': this.isModified,
       'cancelled-color': this.isCancelled
     };
+  }
+  addUser(): any{
+    this.isCorrect = !this.isCorrect;
+    this.usersObject.push({
+      id: 4,
+      name: 'faryad4444'
+    });
+    return this.usersObject;
+  }
+  trackByFunc(index: number, element: any): any{
+    return element.id;
   }
 }
