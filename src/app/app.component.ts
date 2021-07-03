3d3a636185483a7b358de8939f3e30873181a508
Administrator <admin@example.com>
2021-07-03 05:27:37 +0430

    input and outpu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,47 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  isCorrect = false;
-  isModified = false;
-  isCancelled = false;
-  styles = {};
-  classes = {};
 
-  users: string[] = ['faryad', 'farzad', 'payam'];
-
-  usersObject = [
-    {
-      id: 1,
-      name: 'faryad'
-    },
-    {
-      id: 2,
-      name: 'faryad2222'
-    },
-    {
-      id: 3,
-      name: 'faryad33333'
-    },
-  ];
   ngOnInit(): void {
-    this.styles = {
-      'font-size': this.isCorrect ? '2rem' : '8rem',
-      color: 'red'
-    };
-    this.classes = {
-      'big-font-size': this.isModified,
-      'cancelled-color': this.isCancelled
-    };
   }
-  addUser(): any{
-    this.isCorrect = !this.isCorrect;
-    this.usersObject.push({
-      id: 4,
-      name: 'faryad4444'
-    });
-    return this.usersObject;
-  }
-  trackByFunc(index: number, element: any): any{
-    return element.id;
+
+  // tslint:disable-next-line:typedef
+  onStarClicked(rating: number){
+    console.log(rating);
   }
 }
